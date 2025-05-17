a7c88e3c06397a5fc6260b69244ace2217551f27
onefloor <wilsonlo1228@Gmail.com>
2025-05-17 21:44:38 +0800

    更改版本測試 mode =0測試 mode =1是正式

--- rpi-pingpong-detector/src/main.py
+++ rpi-pingpong-detector/src/main.py
@@ -1,27 +1,43 @@
 from camera.pi_camera import PiCamera, WebcamCamera, ImageCamera
 from detection.pingpong_detector import PingPongDetector
 import time
+import cv2
+
 
 def main():
-    #camera = PiCamera()
+    camera = PiCamera()
     #camera = WebcamCamera(camera_id=0)  # Use the first webcam
-    camera = ImageCamera(image_path='image.jpg')  # Use a test image
+    #camera = ImageCamera(image_path='image.jpg')  # Use a test image
     detector = PingPongDetector()
 
     camera.start()
-    
-    try:
-        while True:
+    #mode 0 test
+    #mode 1 真實
+    mode = 0
+    #mode = 1
+
+    if mode == 0:
+        try:
             frame = camera.capture_frame()
-            if detector.detect_ball(frame):
-                print("Ping pong ball detected!")
-            else:
-                print("No ping pong ball detected.")
-            time.sleep(1)  # Adjust the sleep time as needed
-    except KeyboardInterrupt:
-        print("Exiting...")
-    finally:
-        camera.stop()
+            # 儲存 frame 到桌面
+            cv2.imwrite('captured_frame.jpg', frame)
+            print("Frame saved.")
+        finally:
+            camera.stop()
+
+    elif mode == 1:
+        try:
+            while True:
+                frame = camera.capture_frame()
+                if detector.detect_ball(frame):
+                    print("Ping pong ball detected!")
+                else:
+                    print("No ping pong ball detected.")
+                time.sleep(1)  # Adjust the sleep time as needed
+        except KeyboardInterrupt:
+            print("Exiting...")
+        finally:
+            camera.stop()
 
 if __name__ == "__main__":
     main()
