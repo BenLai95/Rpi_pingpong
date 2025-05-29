f269ac92a545c59640db77956d1afe652daa0e6d
onefloor <wilsonlo1228@Gmail.com>
2025-05-29 21:25:57 +0800

    新增DC motor control

--- RPI-ROBOT/src/motors/dc_motor.py
+++ RPI-ROBOT/src/motors/dc_motor.py
@@ -1,29 +1,72 @@
+import RPi.GPIO as GPIO
+import time
+
 class DCMotor:
-    def __init__(self, pin_forward, pin_backward):
-        import RPi.GPIO as GPIO
-        self.pin_forward = pin_forward
-        self.pin_backward = pin_backward
+    def __init__(self, pwm_pin, in1_pin, in2_pin):
+        self.pwm_pin = pwm_pin
+        self.in1_pin = in1_pin
+        self.in2_pin = in2_pin
+
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin_forward, GPIO.OUT)
-        GPIO.setup(self.pin_backward, GPIO.OUT)
-        self.pwm_forward = GPIO.PWM(self.pin_forward, 100)
-        self.pwm_backward = GPIO.PWM(self.pin_backward, 100)
-        self.pwm_forward.start(0)
-        self.pwm_backward.start(0)
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
+        GPIO.setup(self.in1_pin, GPIO.OUT)
+        GPIO.setup(self.in2_pin, GPIO.OUT)
 
-    def start(self):
-        self.pwm_forward.start(100)
-
-    def stop(self):
-        self.pwm_forward.stop()
-        self.pwm_backward.stop()
+        self.pwm = GPIO.PWM(self.pwm_pin, 1000)  # 1kHz PWM
+        self.pwm.start(0)
 
     def set_speed(self, speed):
+        """
+        speed: -100 ~ 100
+        正數為正轉，負數為反轉，0為停止
+        """
         if speed > 0:
-            self.pwm_forward.ChangeDutyCycle(speed)
-            self.pwm_backward.ChangeDutyCycle(0)
+            GPIO.output(self.in1_pin, GPIO.HIGH)
+            GPIO.output(self.in2_pin, GPIO.LOW)
+            self.pwm.ChangeDutyCycle(min(speed, 100))
         elif speed < 0:
-            self.pwm_backward.ChangeDutyCycle(-speed)
-            self.pwm_forward.ChangeDutyCycle(0)
+            GPIO.output(self.in1_pin, GPIO.LOW)
+            GPIO.output(self.in2_pin, GPIO.HIGH)
+            self.pwm.ChangeDutyCycle(min(-speed, 100))
         else:
-            self.stop()
+            GPIO.output(self.in1_pin, GPIO.LOW)
+            GPIO.output(self.in2_pin, GPIO.LOW)
+            self.pwm.ChangeDutyCycle(0)
+
+    def stop(self):
+        self.set_speed(0)
+
+    def cleanup(self):
+        self.pwm.stop()
+        GPIO.cleanup()
+
+class CarController:
+    def __init__(self):
+        # A馬達：PWMA=13, AIN1=26, AIN2=19
+        # B馬達：PWMB=16, BIN1=21, BIN2=20
+        self.motorA = DCMotor(pwm_pin=13, in1_pin=26, in2_pin=19)
+        self.motorB = DCMotor(pwm_pin=16, in1_pin=21, in2_pin=20)
+
+    def forward(self, speed=80):
+        self.motorA.set_speed(speed)
+        self.motorB.set_speed(speed)
+
+    def backward(self, speed=80):
+        self.motorA.set_speed(-speed)
+        self.motorB.set_speed(-speed)
+
+    def left(self, speed=80):
+        self.motorA.set_speed(-speed)
+        self.motorB.set_speed(speed)
+
+    def right(self, speed=80):
+        self.motorA.set_speed(speed)
+        self.motorB.set_speed(-speed)
+
+    def stop(self):
+        self.motorA.stop()
+        self.motorB.stop()
+
+    def cleanup(self):
+        self.motorA.cleanup()
+        self.motorB.cleanup()
