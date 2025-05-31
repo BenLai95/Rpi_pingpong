b058fce896e3e9d8c0f98288a7398f775cdacb93
onefloor <wilsonlo1228@Gmail.com>
2025-05-31 11:32:20 +0800

    ultrasonic test

--- RPI-ROBOT/src/test.py
+++ RPI-ROBOT/src/test.py
@@ -1,10 +1,14 @@
 from motors.servo import ServoMotor
+from sensors.ultrasonic import Ultrasonicsensor
 
 def main():
     servo = ServoMotor(pin=17)  # 根據你的實際接腳調整 pin 編號
+    ultrasonic = Ultrasonicsensor()
     try:
         while True:
-            angle = input("請輸入要旋轉的角度 (0~180，q 離開): ")
+            distance = ultrasonic.get_distance()
+            print(f"距離: {distance:.2f} cm")
+            '''angle = input("請輸入要旋轉的角度 (0~180，q 離開): ")
             if angle.lower() == 'q':
                 break
             try:
@@ -15,7 +19,7 @@ def main():
                 else:
                     print("請輸入 0~180 之間的數字")
             except ValueError:
-                print("請輸入有效的數字或 q 離開")
+                print("請輸入有效的數字或 q 離開")'''
     except KeyboardInterrupt:
         print("\n中斷測試")
     finally:
