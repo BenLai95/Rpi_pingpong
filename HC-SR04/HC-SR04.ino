beca5e3484729f4f40d289e55d74796d3f3159fa
BenLai95 <9.bn5li5sa6dn@gmail.com>
2025-06-08 16:47:11 +0800

    Arduino Code

---
+++ HC-SR04/HC-SR04.ino
@@ -0,0 +1,73 @@
+/*
+請在Arduino IDE - Sketch - 程式庫加入 Ultrasonic 和 Servo 兩個模組
+*/
+#define MotorR_I1 2     // 定義 A1 接腳（右）
+#define MotorR_I2 3     // 定義 A2 接腳（右）
+#define MotorR_PWMR 11  // 定義 ENA (PWM調速) 接腳
+#define MotorL_I3 5     // 定義 B1 接腳（左）
+#define MotorL_I4 6     // 定義 B2 接腳（左）
+#define MotorL_PWML 12  // 定義 ENB (PWM調速) 接腳
+#include <Ultrasonic.h>
+#include <Servo.h>
+Servo myservo;  // 建立一個 servo 物件，最多可建立 12個 servo
+int pos = 0;    // 設定 Servo 位置的變數
+
+Ultrasonic ultrasonic(22, 23);
+int distance;
+
+void MotorWriting(double vL, double vR) {
+  // TODO: use TB6612 to control motor voltage & direction
+  if (vR >= 0) {
+    digitalWrite(MotorR_I1, LOW);
+    digitalWrite(MotorR_I2, HIGH);
+  } else {
+    digitalWrite(MotorR_I1, HIGH);
+    digitalWrite(MotorR_I2, LOW);
+    vR = -vR;
+  }
+  if (vL >= 0) {
+    digitalWrite(MotorL_I3, LOW);
+    digitalWrite(MotorL_I4, HIGH);
+  } else {
+    digitalWrite(MotorL_I3, HIGH);
+    digitalWrite(MotorL_I4, LOW);
+    vL = -vL;
+  }
+  analogWrite(MotorL_PWML, vL);
+  analogWrite(MotorR_PWMR, vR);
+}  // MotorWriting
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(24);
+}
+
+void loop() {
+  // 正轉 180度，從 0 度旋轉到 180 度，每次 1 度
+  for (pos = 0; pos <= 180; pos += 1)  
+  {     
+    myservo.write(pos);    // 告訴 servo 走到 'pos' 的位置
+    delay(15);        // 等待 15ms 讓 servo 走到指定位置
+  }
+
+  // 反轉 180度，從 180 度旋轉到 0 度，每次 1 度 
+  for (pos = 180; pos >= 0; pos -= 1) 
+  { 
+    myservo.write(pos);   // 告訴 servo 走到 'pos' 的位置
+
+    delay(15);   // 等待 15ms 讓 servo 走到指定位置
+  }
+  
+  distance = ultrasonic.read(); //不加參數就是輸出CM，可用read(INC)輸出英寸
+  
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+  delay(500); //每次間格0.5秒
+
+  MotorWriting(50,50);
+  delay(1000);
+  MotorWriting(-50,-50);
+  delay(1000);
+
+  
+}
