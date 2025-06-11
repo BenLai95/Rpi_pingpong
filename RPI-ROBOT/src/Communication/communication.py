c1398ab815c51943c034c01622ba50d743d4655d
BenLai95 <9.bn5li5sa6dn@gmail.com>
2025-06-11 13:13:19 +0800

    print packed

--- RPI-ROBOT/src/Communication/communication.py
+++ RPI-ROBOT/src/Communication/communication.py
@@ -16,6 +16,7 @@ class I2CCommunication:
     def send_float(self, value):
         # 一次送出 4 bytes 小端序 float
         packed = struct.pack('<f', value)
+        print(packed)
         self.bus.write_i2c_block_data(self.address, 0x00, list(packed))
 
     def read_byte(self):
